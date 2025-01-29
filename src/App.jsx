e0ac164ee3a17a6e0826c1e9258ba7d402729fe9
Nayan Desai <ndmaratha01@gmail.com>
2025-01-29 17:06:22 +0530

    Revert "testing"
    This reverts commit ad9057ebf5ce7485f6c3795d7290cec4737a6240.

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,13 @@ import "./App.css";
 export default function App() {
 	return (
 		<BrowserRouter>
-			<div className='min-h-screen w-full bg-gray-100'>
+			<div className='min-h-screen bg-gray-100 text-black border-black'>
 				<Header />
-				<main className='min-h-[calc(100vh-4rem)] w-full p-4 md:p-6 lg:p-8'>
-					<div className='mx-auto max-w-sm sm:max-w-md md:max-w-lg lg:max-w-4xl xl:max-w-7xl'>
-						<Routes>
-							<Route path='/' element={<UserRegistration />} />
-							<Route path='/setup' element={<SetupOrganisation />} />
-							<Route path='/integration' element={<ChatbotIntegration />} />
-						</Routes>
-					</div>
-				</main>
+				<Routes>
+					<Route path='/' element={<UserRegistration />} />
+					<Route path='/setup' element={<SetupOrganisation />} />
+					<Route path='/integration' element={<ChatbotIntegration />} />
+				</Routes>
 			</div>
 		</BrowserRouter>
 	);
